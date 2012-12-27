1c745f67a124338f94efd0d45bdcb791946c9d23
Kaushik Gopal <code@kaush.co>
2012-12-27 16:04:01 +0300

    added a JSON validator and prettied the error output box

--- app/models/device_type.rb
+++ app/models/device_type.rb
@@ -1,4 +1,5 @@
 class DeviceType < ActiveRecord::Base
     attr_accessible :device_type_key, :device_type_desc, :version, :manufacturer, :metadata_json, :default_config
     has_many :devices
+    validates :metadata_json, :json_format => true
 end

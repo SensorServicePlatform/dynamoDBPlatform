72eea2611e3c4eede8df697714f5e99cab2e97cc
Kaushik Gopal <code@kaush.co>
2012-12-28 10:50:34 +0300

    adding json validations for other models with metadata_json

--- app/models/sensor_type.rb
+++ app/models/sensor_type.rb
@@ -1,4 +1,5 @@
 class SensorType < ActiveRecord::Base
     attr_accessible :property_type_key,:property_type_desc, :metadata_json
     has_many :sensors
+    validates :metadata_json, :json_format => true
 end

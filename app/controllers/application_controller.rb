1b372e35e745c697f04b0d21b6f93f91b4f9286c
Yuan Ren <yuan.ren@sv.cmu.edu>
2013-03-11 10:55:28 -0700

    resolve cross domain problem for javascript to call the API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # To resolve cross-domain problem
+  before_filter :cor
+  def cor
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Expose-Headers'] = 'ETag'
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+    headers['Access-Control-Allow-Headers'] = '*, Origin, Accept, X-Requested-With, X-CSRF-Token, Content-Type, If-Modified-Since, If-None-Match'
+    headers['Access-Control-Max-Age'] = '86400'
+    head(:ok) if request.request_method == 'OPTIONS'
+  end
 end
